c806c4a284b4fe9caf1098c72d60b8cd15aec20e
Ben Kempner <ben@verticalbrands.com>
2011-09-09 10:40:42 -0700

    redirect output and errors to log

--- soon.sh
+++ soon.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-PROJ_NAME=$1
-
 # hello
 echo "SOON: $1"
 
-# name
-#echo "Enter project name: "
-#read PROJ_NAME
+# setup
+echo "Setup"
+PROJ_NAME=$1
+LOG_NAME="$PROJ_NAME.soon.log"
 
 # directory
+echo "Creating directory"
 mkdir $PROJ_NAME
 cd $PROJ_NAME
 
 # gemset
-. rvm gemset create $PROJ_NAME
-. rvm gemset use $PROJ_NAME
+echo "Creating gemset"
+. rvm gemset create $PROJ_NAME >> $LOG_NAME 2>&1
+. rvm gemset use $PROJ_NAME >> $LOG_NAME 2>&1
+. rvm gemset list >> $LOG_NAME 2>&1
 echo "rvm gemset use $PROJ_NAME" > .rvmrc
 
 # rails
-gem install rails
-rails new ../$PROJ_NAME --skip-test-unit
+echo "Installing rails"
+gem install rails >> $LOG_NAME 2>&1
+rails new ../$PROJ_NAME --skip-test-unit >> $LOG_NAME 2>&1
 
 # specs
+echo "Installing specs"
 echo "
 group :test, :development do
   gem 'rspec-rails' # rspec for rails
@@ -34,9 +38,10 @@ group :test, :development do
   gem 'pry' # irb replacement
   gem 'gist' # gist code snippets with ease
 end" >> Gemfile
-bundle
-rails g rspec:install
+bundle >> $LOG_NAME 2>&1
+rails g rspec:install >> $LOG_NAME 2>&1
 
+echo "Configuring rails console with pry"
 # rails console with pry
 echo "silence_warnings do
   require 'pry'
@@ -44,10 +49,15 @@ echo "silence_warnings do
 end" >> config/environments/development.rb
 
 # git
-git init
-git add .
-git commit -am 'first commit'
-git log
+echo "Initializing repo"
+git init >> $LOG_NAME 2>&1
+echo "
+*.log
+.DS_STORE
+" >> .gitignore
+git add . >> $LOG_NAME 2>&1
+git commit -am 'first commit' >> $LOG_NAME 2>&1
+git log >> $LOG_NAME 2>&1
 
 # bye
 echo "Project $PROJ_NAME created!"
